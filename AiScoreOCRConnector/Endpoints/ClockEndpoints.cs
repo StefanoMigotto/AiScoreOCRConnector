3f1179f7f81e996433f4e5b765556eed2f8153e3
Stefano Migotto <migottos25@gmail.com>
2025-01-30 14:40:00 +0100

    aggiunta base minimal api + aggiunto endpoint clock

---
+++ AiScoreOCRConnector/Endpoints/ClockEndpoints.cs
@@ -0,0 +1,28 @@
+﻿using AiScoreOCRConnector.Models;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AiScoreOCRConnector.Endpoints
+{
+    public static class ClockEndpoints
+    {
+        public static Clock CurrentClock { get; set; }
+        public static IEndpointRouteBuilder MapClockEndpoints(this IEndpointRouteBuilder app)
+        {
+            var group = app.MapGroup("api/clock");
+
+            group.MapGet("/", GetDataClock);
+            group.MapPost("/", InsertDataClock);
+
+            return app;
+        }
+        public static Results<Ok<Clock>, ProblemHttpResult> GetDataClock()
+        {
+            return TypedResults.Ok(CurrentClock);
+        }
+        public static void InsertDataClock([FromBody]Clock payload)
+        {
+            CurrentClock = payload;
+        }
+    }
+}
---
+++ AiScoreOCRConnector/Models/Clock.cs
@@ -0,0 +1,7 @@
+﻿namespace AiScoreOCRConnector.Models
+{
+    public class Clock
+    {
+        public string ClockValue { get; set; }
+    }
+}
